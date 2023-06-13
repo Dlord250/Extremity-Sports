74ea42bef32cd160150d4455f4df41ceed3f72bb
David Ferreira da Costa <120753154+Dlord250@users.noreply.github.com>
2023-06-13 20:56:13 -0300

    Add files via upload

--- src/js/style.js
+++ src/js/style.js
@@ -9,9 +9,22 @@ sr.reveal ('.logo-junta',{
     rotate:{x: 100,}
 });
 
-sr.reveal ('.descricao',{duration: 5000});
+// sr.reveal ('.descricao',{duration: 5000});
 sr.reveal ('.contato.aberto',{duration: 1500})
+sr.reveal ('.free-fire',{duration: 5000})
+sr.reveal ('.speed-drifters',{duration: 3000})
+sr.reveal ('.clash-of-clans',{duration: 7000})
 
+const descricao = document.querySelector('.descricao');
+typeWriter(descricao)
+
+function typeWriter(elemento) {
+    const textoArray =elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) =>{
+        setTimeout (()=> elemento.innerHTML += letra, 75 * i);
+    });
+}
 
 
 
