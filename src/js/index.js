834fb4fcbbb12cb08b99ed51e13d9ae722fe6ab1
David Ferreira da Costa <120753154+Dlord250@users.noreply.github.com>
2023-07-02 01:00:55 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -4,26 +4,26 @@
 //1. Quando usuario clikar no botao mostrar o quadro
 
 //Passo 1. dar um jeito de pega elemento e coloca no js
-const botaoJunte = document.querySelector('.botao-junte')
+//
 
 //Passo 2. identificar click no botao
-botaoJunte.addEventListener('click', () => {
+//botaoJunte.addEventListener('click', () => {
  //Passo 4. abrir contato na tela
-contato.classList.add('aberto')});
+//contato.classList.add('aberto')});
    
 
 //Passo 3. dar um jeito de pegar o elemento da modal no js
-const contato = document.querySelector('.contato');
+//const contato = document.querySelector('.contato');
     
    
 //2.quando usuario clikar no X fecha o aviso
 
 //Passo 1. dar um jeito de pega elemento e coloca no js
-const fecharContato = document.querySelector('.fechar-contato'); 
+//const fecharContato = document.querySelector('.fechar-contato'); 
 //2. identificar click no botao
-fecharContato.addEventListener('click', ()=> {
+//fecharContato.addEventListener('click', ()=> {
     //Passo 3. fechar contato na tela
-contato.classList.remove('aberto'); })
+//contato.classList.remove('aberto'); })
 
 
 // const telalogin = document.querySelector('.login')
